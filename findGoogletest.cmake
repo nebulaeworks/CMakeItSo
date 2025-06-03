1e51ab99df0710548b22b5dc58c9af21d5c2936d
Jan Hanson <jpchansondev@gmail.com>
2025-06-03 17:31:44 +0100

    WIP: initial draft

--- findGoogletest.cmake
+++ findGoogletest.cmake
@@ -10,5 +10,25 @@ ELSE (${GTEST_FOUND})
         DOWNLOAD_EXTRACT_TIMESTAMP true
         FIND_PACKAGE_ARGS NAMES GTest
     )
+
     FETCHCONTENT_MAKEAVAILABLE(googletest)
+    set(GTEST_ALIAS_TARGETS
+        GTest::gtest
+        GTest::gtest_main
+        GTest::gmock
+        GTest::gmock_main
+    )
+
+    foreach(alias IN LISTS GTEST_ALIAS_TARGETS)
+        if (TARGET ${alias})
+            # Resolve to actual target name
+            get_target_property(real_target ${alias} NAME)
+            if (NOT ${alias} STREQUAL ${real_target})
+                set_target_properties(${real_target} PROPERTIES
+                    ARCHIVE_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/tests"
+                    LIBRARY_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/tests"
+                )
+            endif()
+        endif()
+    endforeach()
 ENDIF (${GTEST_FOUND})
